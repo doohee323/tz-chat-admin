ddcca331cf04ebbc744ceb316b0696095ff64d31
Hong, Dewey <doohee323@gmail.com>
2016-10-15 23:44:53 -0700

    .

--- web_apps/application/views/templates/index.php
+++ web_apps/application/views/templates/index.php
@@ -45,7 +45,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <header class="main-header">
 
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="/main" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>Tz</b>Chat</span>
       <!-- logo for regular state and mobile devices -->
